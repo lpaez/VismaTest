d3bbd577ac7efea867525e79dba2c634b0656824
Laureano Paez <laureano.paez@mediware.com>
2020-10-12 22:10:53 -0300

    Added DataAnnotations to mandatory employee properties

--- VismaTest/Models/Employee.cs
+++ VismaTest/Models/Employee.cs
@@ -1,10 +1,15 @@
-﻿namespace VismaTest.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VismaTest.Models
 {
     public class Employee
     {
         public int EmployeeId { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string SSN { get; set; }
         public string PhoneNumber { get; set; }
     }
